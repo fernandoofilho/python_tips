f4deca51b43a66b2b54924d1b125637f2d29bfbf
Fernando Filho <faaf.eng19@uea.edu.br>
2024-04-14 23:25:11 -0400

    strings tips

---
+++ strings/concatenar_strings.py
@@ -0,0 +1,12 @@
+# Concatenação com operador +
+words = ["hello", "world", "python"]
+result = ""
+for word in words:
+    result += word + " "
+# hello world python
+
+# Concatenação com método join()
+words = ["hello", "world", "python"]
+result = " ".join(words)
+
+# hello world python
---
+++ strings/remover_espacos_strings.py
@@ -0,0 +1,4 @@
+s = "   Hello, World!   "
+cleaned_string = s.strip()
+
+print(cleaned_string)  # Output: "Hello, World!"
