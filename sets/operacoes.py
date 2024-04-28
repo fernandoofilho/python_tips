7d0bab224ff404596007f9b2df61c2860834eec9
Fernando Filho <faaf.eng19@uea.edu.br>
2024-04-27 23:44:39 -0400

    operacoes com sets

---
+++ sets/operacoes.py
@@ -0,0 +1,14 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+uniao = set1.union(set2)
+# {1, 2, 3, 4, 5}
+
+intersecao = set1.intersection(set2)
+# {3}
+
+diferenca = set1.difference(set2)
+# {1, 2}
+
+dif_simetrica = set1.symmetric_difference(set2)
+# {1, 2, 4, 5}
+
